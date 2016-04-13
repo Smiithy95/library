e347f44567af0871ec54d626158dac43c456a835
Smiithy95 <b4025516@my.shu.ac.uk>
2016-04-14 00:41:35 +0100

    Created Book class and implemented scaffolding

---
+++ library/grails-app/domain/com/library/Book.groovy
@@ -0,0 +1,35 @@
+package com.library
+
+class Book {
+
+String title
+
+String author
+
+String isbn
+
+Date dateBorrowed
+
+Date returnDate
+
+String student
+
+String toString(){
+ "$title, $author"}
+
+    static constraints = {
+
+title()
+
+author()
+
+isbn()
+
+dateBorrowed()
+
+returnDate()
+
+student()
+
+    }
+}
---
+++ library/grails-app/domain/com/library/Library.groovy
@@ -0,0 +1,29 @@
+package com.library
+
+class Library {
+
+ String location
+
+ String openingHours
+
+ String book
+
+ String student
+
+ String librarian
+
+ String toString(){"$location, $librarian, $openingHours"}
+
+    static constraints = {
+
+location()
+
+openingHours()
+
+book()
+
+student()
+
+librarian()
+    }
+}
