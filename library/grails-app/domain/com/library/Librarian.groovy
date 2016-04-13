0f09682290bae5ed73fe8105f9c532a48c2282fc
Smiithy95 <b4025516@my.shu.ac.uk>
2016-04-13 23:33:22 +0100

    Modelled the Librarian class in scaffolding view

---
+++ library/grails-app/domain/com/library/Librarian.groovy
@@ -0,0 +1,18 @@
+package com.library
+
+class Librarian {
+
+String name
+
+String email
+
+String username
+
+String password
+
+String telephone
+
+
+String toString(){"$name, $email, $telephone"}
+
+}
